60ba41f68249c6ffa3e6ccbd1da346eb4148e1b2
Rajeev Balendra <rj.balendra@gmail.com>
2021-05-12 21:31:23 +1000

    learnt about rails credentials

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,9 +1,8 @@
 class Stock < ApplicationRecord
   
   def self.new_lookup(ticker_symbol)
-    client = IEX::Api::Client.new(publishable_token: 'Tpk_86383a1fb1fb441fb9a0f102a1fea768',
-                                        secret_token: 'Tsk_035b6e28e49443e7bfb13dcf3f00c116',
-                                            endpoint: 'https://sandbox.iexapis.com/stable')
+    client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
+                                        endpoint: 'https://sandbox.iexapis.com/stable')
       client.price(ticker_symbol)
   end
 
