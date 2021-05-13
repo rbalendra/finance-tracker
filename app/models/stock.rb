afefd988cfb855f7551abe7f085abb95ff727ecc
Rajeev Balendra <rj.balendra@gmail.com>
2021-05-13 23:47:53 +1000

    validations

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -5,8 +5,10 @@ class Stock < ApplicationRecord
                                         endpoint: 'https://sandbox.iexapis.com/stable')
   
 
-    new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol) )                                  
-  
+    begin
+      new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.price(ticker_symbol) )                                  
+    rescue => exception
+      return nil
+    end
   end
-
 end
