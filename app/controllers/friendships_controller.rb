52445185930a7d1b4289b1092f1282a1c69aa83a
Rajeev Balendra <rj.balendra@gmail.com>
2021-05-18 22:41:58 +1000

    Display friends show page partial

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,12 +1,19 @@
 class FriendshipsController < ApplicationController
   def create
-
+    friend = User.find(params[:friend])
+    current_user.friendships.build(friend_id: friend.id)
+    if current_user.save
+      flash[:notice] = "following friend"
+    else
+      flash[:alert] = "There was something wrong with the request"
+    end
+    redirect_to my_friends_path
   end
 
   def destroy
     friendship = current_user.friendships.where(friend_id: params[:id]).first
     friendship.destroy
     flash[:notice] = "stopped following"
-    redirect_to_my_friends_path
+    redirect_to my_friends_path
   end
 end
